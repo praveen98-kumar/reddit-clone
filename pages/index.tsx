f5235228fd34bb38efc0d188602159f108afeed2
praveen98-kumar <58694592+praveen98-kumar@users.noreply.github.com>
2021-01-23 13:05:57 +0530

    Auth (Github and Google using next-auth) (#2)
    * typescript setup done
    
    * tailwindcss added
    
    * prisma & db connection setup
    
    * env in gitignore
    
    * env in gitignore
    
    * env
    
    * github login setup
    
    * google auth done

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,7 +1,14 @@
 import Head from "next/head";
-// import styles from '../styles/Home.module.css'
+
+import { signIn, signOut, useSession } from "next-auth/client";
 
 export default function Home() {
+  const [session, loading] = useSession();
+
+  if (loading) {
+    return <div>Loading...</div>;
+  }
+
   return (
     <div>
       <Head>
@@ -9,15 +16,43 @@ export default function Home() {
         <link rel="icon" href="/favicon.ico" />
       </Head>
 
-      <main>
+      <main className="space-y-6 text-center">
         <h1 className="text-5xl text-font-bold">
-          Welcome to <a href="https://nextjs.org" className="text-blue-500">Next.js!</a>
+          Welcome to{" "}
+          <a href="https://nextjs.org" className="text-blue-500">
+            Next.js!
+          </a>
         </h1>
+        <div>
+          {session && (
+            <p>
+              Signed in as:{" "}
+              <span className="font-bold text-blue-300">
+                {session.user.name}
+              </span>
+            </p>
+          )}
+          <div>
+            {!session ? (
+              <button
+                onClick={() => signIn()}
+                className="px-3 py-1 ml-2 rounded-lg text-white bg-blue-400 hover:bg-blue-500 focus:outline-none"
+              >
+                Sign In
+              </button>
+            ) : (
+              <>
+                <button
+                  onClick={() => signOut()}
+                  className="px-3 py-1 ml-2 rounded-lg text-white bg-red-500 hover:bg-red-700 focus:outline-none"
+                >
+                  Sign Out
+                </button>
+              </>
+            )}
+          </div>
+        </div>
 
-        <p>
-          Get started by editing{" "}
-          <code className="text-red-500">pages/index.js</code>
-        </p>
 
         <div></div>
       </main>
