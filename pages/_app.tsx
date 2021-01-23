0c11629f2d96e43df8cee8981fae837e0c2ee240
praveen98-kumar <58694592+praveen98-kumar@users.noreply.github.com>
2021-01-23 15:43:55 +0530

    Test (#3)
    * typescript setup done
    
    * tailwindcss added
    
    * prisma & db connection setup
    
    * env in gitignore
    
    * env in gitignore
    
    * env
    
    * github login setup
    
    * google auth done
    
    * register testing

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,12 +1,11 @@
 import "../styles/globals.css";
 import { AppProps } from "next/app";
-import { Provider } from "next-auth/client";
+
 
 function MyApp({ Component, pageProps }: AppProps) {
   return (
-    <Provider session={pageProps.session}>
       <Component {...pageProps} />
-    </Provider>
+
   );
 }
 
